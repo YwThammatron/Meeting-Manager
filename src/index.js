f9691b0dd8475921d41206f7baaafbb01f60b0ce
YwThammatron <thammatron135@gmail.com>
2025-10-30 10:59:18 +0700

    remove config.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const { Client } = require("discord.js")
+const { Client, IntentsBitField } = require("discord.js")
 const { token } = require("./config.json")
 
-const client = new Client({intents : []})
+const client = new Client({
+    intents : [
 
+    ]
+})
 
+client.on('clientReady',(c) => {
+    console.log(`${c.user.tag} is ready`)
+})
+
+client.on()
 
 client.login(token)
 
